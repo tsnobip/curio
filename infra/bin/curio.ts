bd633a6f25a33f82b872640831a6ad3e2ec7917c
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2026-03-27 08:51:04 +0100

    fix ECR region (#4)

--- infra/bin/curio.ts
+++ infra/bin/curio.ts
@@ -7,6 +7,6 @@ const app = new cdk.App();
 new CurioStack(app, "CurioStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION ?? "us-west-1",
+    region: process.env.CDK_DEFAULT_REGION ?? "eu-west-1",
   },
 });
