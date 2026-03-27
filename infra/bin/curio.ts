c454a024f457ddb25cecda56104803301a858e82
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2026-03-27 09:23:06 +0100

    use cross region certificate (#6)

--- infra/bin/curio.ts
+++ infra/bin/curio.ts
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+import { CertificateStack } from "../lib/certificate-stack";
 import { CurioStack } from "../lib/curio-stack";
 
 const app = new cdk.App();
 
+const account = process.env.CDK_DEFAULT_ACCOUNT;
+const region = process.env.CDK_DEFAULT_REGION ?? "eu-west-1";
+
+const certStack = new CertificateStack(app, "CurioCertificateStack", {
+  env: { account, region: "us-east-1" },
+  crossRegionReferences: true,
+});
+
 new CurioStack(app, "CurioStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION ?? "eu-west-1",
-  },
+  env: { account, region },
+  crossRegionReferences: true,
+  certificate: certStack.certificate,
+  hostedZoneId: certStack.hostedZoneId,
+  hostedZoneName: certStack.hostedZoneName,
 });
